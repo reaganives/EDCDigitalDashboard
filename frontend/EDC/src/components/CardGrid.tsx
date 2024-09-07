b4e388c7e63e036ad4335df747c5892c6989c4e7
Reagan Ives <reagan.ives@protonmail.com>
2024-09-07 16:32:19 -0700

    opacity

--- frontend/EDC/src/components/CardGrid.tsx
+++ frontend/EDC/src/components/CardGrid.tsx
@@ -37,7 +37,7 @@ const fadeInVariants = {
     opacity: 1,
     transition: {
       duration: 1,
-      delay: 1, // 2-second delay before the fade-in
+      delay: 1,
     },
   },
 };
@@ -46,19 +46,12 @@ export default function CardGrid() {
   const [loading, setLoading] = useState(true); // Track loading state
 
   useEffect(() => {
-    // Add background class on mount
-    document.body.classList.add('bg-gray-100');
-
-    // Set loading to false after 1.5 seconds
+    // Set loading to false after 2.25 seconds (or when data is done loading)
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1750);
+    }, 2250);
 
-    // Clean up function to remove background class
-    return () => {
-      clearTimeout(timer);
-      document.body.classList.remove('bg-gray-100');
-    };
+    return () => clearTimeout(timer);
   }, []);
 
   return (
@@ -66,127 +59,125 @@ export default function CardGrid() {
       {/* Show the Loading animation until loading is finished */}
       {loading && <Loading />}
       
-      {/* Render the CardGrid after the loading state is set to false */}
-      {!loading && (
-        <div className="flex flex-col justify-center items-center">
-          <div className="grid lg:grid-cols-4 lg:grid-rows-2 grid-cols-2 grid-rows-4 auto-rows-auto md:gap-8 gap-4 w-full lg:max-w-6xl sm:max-w-lg md:px-8 px-2 flex-grow">
-            {/* Desktop View */}
-            <motion.div
-              className="lg:col-span-1 lg:row-span-2 hidden lg:grid"
-              custom="down"
-              initial="hidden"
-              animate="visible"
-              variants={cardVariants}
-            >
-              <MLBCard />
-            </motion.div>
-
-            <motion.div
-              className="lg:col-span-1 lg:row-span-1 hidden lg:grid"
-              custom="up"
-              initial="hidden"
-              animate="visible"
-              variants={cardVariants}
-            >
-              <LichessCard />
-            </motion.div>
-
-            <motion.div
-              className="lg:col-span-2 lg:row-span-1 hidden lg:grid"
-              custom="right"
-              initial="hidden"
-              animate="visible"
-              variants={cardVariants}
-            >
-              <SpotifyCard />
-            </motion.div>
-
-            <motion.div
-              className="lg:col-span-2 lg:row-span-1 hidden lg:grid"
-              custom="left"
-              initial="hidden"
-              animate="visible"
-              variants={cardVariants}
-            >
-              <XCard />
-            </motion.div>
-
-            <motion.div
-              className="lg:col-span-1 lg:row-span-1 hidden lg:grid"
-              custom="down"
-              initial="hidden"
-              animate="visible"
-              variants={cardVariants}
-            >
-              <WeatherCard />
-            </motion.div>
-
-            {/* Mobile View */}
-            <motion.div
-              className="col-span-2 row-span-1 grid lg:hidden"
-              custom="right"
-              initial="hidden"
-              animate="visible"
-              variants={cardVariants}
-            >
-              <SpotifyCard />
-            </motion.div>
-
-            <motion.div
-              className="col-span-1 row-span-2 grid lg:hidden"
-              custom="down"
-              initial="hidden"
-              animate="visible"
-              variants={cardVariants}
-            >
-              <MLBCard />
-            </motion.div>
-
-            <motion.div
-              className="col-span-1 row-span-1 grid lg:hidden"
-              custom="left"
-              initial="hidden"
-              animate="visible"
-              variants={cardVariants}
-            >
-              <LichessCard />
-            </motion.div>
-
-            <motion.div
-              className="col-span-1 row-span-1 grid lg:hidden"
-              custom="right"
-              initial="hidden"
-              animate="visible"
-              variants={cardVariants}
-            >
-              <WeatherCard />
-            </motion.div>
-
-            <motion.div
-              className="col-span-2 row-span-1 grid lg:hidden"
-              custom="left"
-              initial="hidden"
-              animate="visible"
-              variants={cardVariants}
-            >
-              <XCard />
-            </motion.div>
-          </div>
-
+      {/* Render the CardGrid immediately but keep it hidden while loading */}
+      <div
+        className={`flex flex-col justify-center items-center transition-opacity duration-500 ${loading ? 'opacity-0' : 'opacity-100'}`}
+      >
+        <div className="grid lg:grid-cols-4 lg:grid-rows-2 grid-cols-2 grid-rows-4 auto-rows-auto md:gap-8 gap-4 w-full lg:max-w-6xl sm:max-w-lg md:px-8 px-2 flex-grow">
+          {/* Desktop View */}
           <motion.div
-            className="mt-8 flex justify-center w-full"
+            className="lg:col-span-1 lg:row-span-2 hidden lg:grid"
+            custom="down"
             initial="hidden"
             animate="visible"
-            variants={fadeInVariants} // Apply fade-in variants here
+            variants={cardVariants}
           >
-            <div className="lg:max-w-5xl max-w-lg">
-              <CryptoCarousel />
-            </div>
+            <MLBCard />
+          </motion.div>
+
+          <motion.div
+            className="lg:col-span-1 lg:row-span-1 hidden lg:grid"
+            custom="up"
+            initial="hidden"
+            animate="visible"
+            variants={cardVariants}
+          >
+            <LichessCard />
+          </motion.div>
+
+          <motion.div
+            className="lg:col-span-2 lg:row-span-1 hidden lg:grid"
+            custom="right"
+            initial="hidden"
+            animate="visible"
+            variants={cardVariants}
+          >
+            <SpotifyCard />
+          </motion.div>
+
+          <motion.div
+            className="lg:col-span-2 lg:row-span-1 hidden lg:grid"
+            custom="left"
+            initial="hidden"
+            animate="visible"
+            variants={cardVariants}
+          >
+            <XCard />
+          </motion.div>
+
+          <motion.div
+            className="lg:col-span-1 lg:row-span-1 hidden lg:grid"
+            custom="down"
+            initial="hidden"
+            animate="visible"
+            variants={cardVariants}
+          >
+            <WeatherCard />
+          </motion.div>
+
+          {/* Mobile View */}
+          <motion.div
+            className="col-span-2 row-span-1 grid lg:hidden"
+            custom="right"
+            initial="hidden"
+            animate="visible"
+            variants={cardVariants}
+          >
+            <SpotifyCard />
+          </motion.div>
+
+          <motion.div
+            className="col-span-1 row-span-2 grid lg:hidden"
+            custom="down"
+            initial="hidden"
+            animate="visible"
+            variants={cardVariants}
+          >
+            <MLBCard />
+          </motion.div>
+
+          <motion.div
+            className="col-span-1 row-span-1 grid lg:hidden"
+            custom="left"
+            initial="hidden"
+            animate="visible"
+            variants={cardVariants}
+          >
+            <LichessCard />
+          </motion.div>
+
+          <motion.div
+            className="col-span-1 row-span-1 grid lg:hidden"
+            custom="right"
+            initial="hidden"
+            animate="visible"
+            variants={cardVariants}
+          >
+            <WeatherCard />
+          </motion.div>
+
+          <motion.div
+            className="col-span-2 row-span-1 grid lg:hidden"
+            custom="left"
+            initial="hidden"
+            animate="visible"
+            variants={cardVariants}
+          >
+            <XCard />
           </motion.div>
         </div>
-      )}
+
+        <motion.div
+          className="mt-8 flex justify-center w-full"
+          initial="hidden"
+          animate="visible"
+          variants={fadeInVariants} // Apply fade-in variants here
+        >
+          <div className="lg:max-w-5xl max-w-lg">
+            <CryptoCarousel />
+          </div>
+        </motion.div>
+      </div>
     </div>
   );
 }
-
-
