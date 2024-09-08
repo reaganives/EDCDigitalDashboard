51e3516ef4ccf744cac3272585a2b4b4ff4c8874
Reagan Ives <reagan.ives@protonmail.com>
2024-09-07 18:04:24 -0700

    loader

--- frontend/EDC/src/components/Loading.tsx
+++ frontend/EDC/src/components/Loading.tsx
@@ -3,8 +3,8 @@ import animationData from '../assets/loading.json'; // Import your JSON file
 
 export default function Loading() {
   return (
-    <div className='w-full h-full flex justify-center items-center'>
-      <Lottie animationData={animationData} loop={true} style={{ width: '450px', height: '450px' }} />
+    <div className='w-full h-full flex justify-center items-center pt-16'>
+      <Lottie animationData={animationData} loop={true} style={{ width: '310px', height: '310px' }} />
     </div>
   );
 }
