96c9ba3b2689f4ee5c2cfe7d3eb75237dc211464
Reagan Ives <reagan.ives@protonmail.com>
2024-09-10 12:15:32 -0700

    github icon

--- frontend/EDC/src/App.tsx
+++ frontend/EDC/src/App.tsx
@@ -2,20 +2,25 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SpotifyAuth from './components/SpotifyAuth';
 import SpotifyCallback from './components/SpotifyCallback';
 import CardGrid from './components/CardGrid';
+import Github from './components/Github'; // Import Github component
 import './App.css';
 import './index.css';
 
 function App() {
     return (
         <Router>
+            {/* Github Button added here */}
+            <Github url="https://github.com/reaganives/EDC" />
+
+            {/* Routes for different components */}
             <Routes>
                 <Route path="/spotify/auth" element={<SpotifyAuth />} />
                 <Route path="/spotify/callback" element={<SpotifyCallback />} />
                 <Route path="/" element={
-                  <div className='lg:mt-[25vh] mt-[10vh]'>
-                    <h1 className='md:text-6xl text-5xl font-bold text-center tracking-wide mb-10'></h1>
-                    <CardGrid />
-                  </div>
+                    <div className='lg:mt-[25vh] mt-[10vh]'>
+                        <h1 className='md:text-6xl text-5xl font-bold text-center tracking-wide mb-10'></h1>
+                        <CardGrid />
+                    </div>
                 } />
             </Routes>
         </Router>
---
+++ frontend/EDC/src/components/Github.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+interface GithubProps {
+  url: string;
+}
+
+const Github: React.FC<GithubProps> = ({ url }) => {
+  const handleClick = () => {
+    window.open(url, '_blank');
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className="absolute top-4 right-4 p-2 hover:bg-gray-200 rounded-full transition-all duration-200 ease-in-out"
+      aria-label="Github"
+    >
+      <FaGithub size={30} />
+    </button>
+  );
+};
+
+export default Github;
